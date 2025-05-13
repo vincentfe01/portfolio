2c2b364ec0bf315134aa12ec72473d326bd1cd20
Le <leoaivy@macbookair.home>
2025-05-13 10:17:28 -0400

    chore: update components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import {Spotlight} from "@/components/ui/Spotlight";
 import {cn} from "@/utils/cn";
 import {TextGenerateEffect} from "@/components/ui/TextGenerateEffect";
 import MagicButton from "@/components/ui/MagicButton";
+import {FaLocationArrow} from "react-icons/fa6";
 
 const Hero = () => {
   return (
@@ -30,13 +31,15 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
-          <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless Experiences" />
+          <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless Users Experiences" />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&#39;m Vincent, a Next.js Developer based in Canada.
           </p>
           <a href="#about" >
             <MagicButton
               title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
             />
           </a>
         </div>
---
+++ components/ui/BentoGrid.tsx
@@ -0,0 +1,57 @@
+import { cn } from "@/utils/cn";
+
+export const BentoGrid = ({
+                            className,
+                            children,
+                          }: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+                                className,
+                                title,
+                                description,
+                                header,
+                                icon,
+                                id,
+                              }: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  header?: React.ReactNode;
+  icon?: React.ReactNode;
+  id: number
+}) => {
+  return (
+    <div
+      id={id}
+      className={cn(
+        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none border-red-500 relative",
+        className,
+      )}
+    >
+      {header}
+      <div className="transition duration-200 group-hover/bento:translate-x-2">
+        {icon}
+        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+          {title}
+        </div>
+        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
