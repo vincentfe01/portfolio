3e6fa5b543f6077366f963c65c04cd59cc199e77
Vincent Le <vincentle@imac-de-vincent.home>
2025-05-16 16:33:32 -0400

    chore: update new things

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -5,16 +5,16 @@ import {FaLocationArrow} from "react-icons/fa6";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20 ">
+    <div className="py-20 " id="projects">
       <h1 className="heading">
         A small selection of {' '}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
-          <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
+          <div key={id} className="sm:h-[41rem] h-[32rem] sm:w-[570px] lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
             <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] h-[20vh] lg:h-[30vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-96 sm:w-[570px] w-[80vw] sm:h-[40vh] h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
---
+++ instrumentation-client.ts
@@ -0,0 +1,24 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://37e9236122061bb079432144b9979aed@o4509333775450112.ingest.us.sentry.io/4509333782462464",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  integrations: [
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "dark",
+    }),
+  ],
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
